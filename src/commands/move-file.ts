f2afe75539d5ce44e91b4b99128d4dce8ce80632
Henrique <henryqueoli@hotmail.com>
2022-03-20 18:26:56 -0300

    feat: add move and restore commands

---
+++ src/commands/move-file.ts
@@ -0,0 +1,26 @@
+import Directory from "@/models/Directory";
+import { defineCommand } from "@/utils/command";
+
+export default defineCommand({
+    name: "move-file",
+    description: "Move a file from archive source to target",
+    async execute({ args, prisma }) {
+        const id = Number(args[0]);
+        const filename = args[1];
+
+        const archive = await prisma.archive.findFirst({
+            where: { id },
+        });
+
+        if (!archive) {
+            throw new Error("Archive not found");
+        }
+
+        const source = new Directory(archive.source);
+        const target = new Directory(archive.target);
+
+        await source.move(filename, target);
+
+        console.log("File moved successfully");
+    },
+});
--- src/models/Directory.ts
+++ src/models/Directory.ts
@@ -22,4 +22,27 @@ export default class Directory {
     public print() {
         this.items.forEach((item) => console.log(`${item.icon} ${item.name}`));
     }
+
+    public async move(name: string, directory: Directory) {
+        const item = this.items.find((item) => item.name === name);
+
+        if (!item) {
+            throw new Error("File not found");
+        }
+
+        const oldPath = path.resolve(this.rootPath, name);
+        const newPath = path.resolve(directory.rootPath, name);
+
+        return fs.promises
+            .rename(oldPath, newPath)
+            .then(() => {
+                this.refresh();
+                directory.refresh();
+                return true;
+            })
+            .catch((err) => {
+                console.error(err);
+                return false;
+            });
+    }
 }
