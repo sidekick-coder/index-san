6c2b10ce3cf25f7acfb30bd7c735a7dcd20a3635
Henrique <henryqueoli@hotmail.com>
2022-03-20 18:05:34 -0300

    feat: add tsconfig-paths

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
+const tsConfig = require("./tsconfig.json");
 
 require("ts-node").register({});
+require("tsconfig-paths").register({
+    baseUrl: tsConfig.compilerOptions.baseUrl,
+    paths: tsConfig.compilerOptions.paths,
+});
 
 require("./src/cli.ts");
