aefd52b73f650ec4359f26672d18f591b2b5f7fe
henrique <henriqueoliwork@gmail.com>
2022-09-16 16:24:24 -0300

    fix(desktop): eslint parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,5 +38,6 @@ module.exports = {
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
         '@typescript-eslint/ban-types': 'off',
+        'vue/multi-word-component-names': 'off'
     }
 }
