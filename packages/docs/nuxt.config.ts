21b0f08ba4a02dfc171fecc863434baa6b1c41a7
henrique <henriqueoliwork@gmail.com>
2023-01-21 17:15:10 -0300

    feat(docs): add VBtn page

--- packages/docs/nuxt.config.ts
+++ packages/docs/nuxt.config.ts
@@ -1,7 +1,38 @@
+import path from 'path'
+
+const desktopPath = path.resolve(__dirname, '..', 'desktop')
+
 export default defineNuxtConfig({
     extends: '@nuxt-themes/docus',
+    modules: ['@nuxtjs/tailwindcss'],
     content: {
         locales: ['en-US', 'pt-BR'],
         defaultLocale: 'en-US',
+    },
+    components: {
+        dirs: [
+            '~/components',
+            {
+                path: path.resolve(desktopPath, 'components'),
+                global: true,
+            },
+        ],
+    },
+    alias: {
+        '@core': path.resolve(__dirname, '..', 'core'),
+        // app
+        '@components': path.resolve(desktopPath, 'components'),
+        '@composables': path.resolve(desktopPath, 'composables'),
+        '@plugins': path.resolve(desktopPath, 'plugins'),
+        '@modules': path.resolve(desktopPath, 'modules'),
+        '@store': path.resolve(desktopPath, 'store'),
+    },
+    tailwindcss: {
+        cssPath: path.resolve(__dirname, '..', 'desktop', 'assets', 'tailwind.scss'),
+        configPath: path.resolve(desktopPath, 'tailwind.config.js'),
+        injectPosition: 'last',
+    },
+    pinceau: {
+        preflight: false,
     },
 })
