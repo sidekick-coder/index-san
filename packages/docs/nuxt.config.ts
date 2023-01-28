c4f2c04ea71eb29d32c67cc03c6da2bd9b19b384
henrique <henriqueoliwork@gmail.com>
2023-01-28 11:49:36 -0300

    fix(docs): ignore components test files

--- packages/docs/nuxt.config.ts
+++ packages/docs/nuxt.config.ts
@@ -15,6 +15,7 @@ export default defineNuxtConfig({
             {
                 path: path.resolve(desktopPath, 'components'),
                 global: true,
+                ignore: ['*.spec.ts'],
             },
         ],
     },
