55de2c4badc3b25ecd4345c8405c56c0e57685a6
henrique <henriqueoliwork@gmail.com>
2023-05-12 13:30:21 -0300

    refactor(app): remove redirect from ELayout

--- packages/app/modules/entry/ELayout.vue
+++ packages/app/modules/entry/ELayout.vue
@@ -1,38 +1,9 @@
 <script setup lang="ts">
-import DirectoryEntry from '@core/entities/directory-entry'
-import { watch } from 'vue'
-import { useRouter } from 'vue-router'
-
-import { useStore } from '@store/global'
-
-const props = defineProps({
+defineProps({
     entryId: {
         type: String,
         default: '/',
     },
-})
-
-// check if is item
-const store = useStore()
-const router = useRouter()
-
-function check() {
-    const basename = DirectoryEntry.basename(props.entryId)
-    const dirname = DirectoryEntry.dirname(props.entryId)
-
-    if (basename === '.is') return
-
-    const collection = store.collection.collections.find(
-        (c) => DirectoryEntry.normalize(c.path) === dirname
-    )
-
-    if (collection) {
-        router.push(`/collections/${collection.id}/items/${basename}`)
-    }
-}
-
-watch(() => props.entryId, check, {
-    immediate: true,
 })
 </script>
 
