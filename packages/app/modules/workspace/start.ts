f44f940c770318779c821113a2bcf7dc57ed4350
henrique <henriqueoliwork@gmail.com>
2023-06-03 17:10:50 -0300

    fix(app): menu start errors

--- packages/app/modules/workspace/start.ts
+++ packages/app/modules/workspace/start.ts
@@ -4,4 +4,10 @@ export default async () => {
     const store = useStore()
 
     await store.setWorkspaces()
+
+    if (store.currentId) return
+
+    if (!store.workspaces.length) return
+
+    store.currentId = store.workspaces[0].id
 }
