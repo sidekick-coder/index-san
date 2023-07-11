b06869a227a6f1dc396c394511b81bc4a9152551
henrique <henriqueoliwork@gmail.com>
2023-07-11 11:37:10 -0300

    fix(electron): electron forge not working

--- packages/electron/src-electron/fetch.ts
+++ packages/electron/src-electron/fetch.ts
@@ -1,9 +1,9 @@
 import type IFetchService from '@is/core/gateways/fetch/fetch'
 
-import nodeFetch from 'node-fetch'
+import * as nodeFetch from 'node-fetch'
 
 export default class FetchService implements IFetchService {
     public provide() {
-        return nodeFetch as typeof fetch
+        return nodeFetch as any
     }
 }
