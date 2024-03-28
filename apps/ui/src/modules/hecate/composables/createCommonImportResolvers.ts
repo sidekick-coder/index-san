0ce8ddcc551388890aa80306b460781fbd507c4a
Henrique de Oliveira <henriqueoliwork@gmail.com>
2024-03-28 20:31:44 -0300

    feat(ui): add drive import resolver

--- apps/ui/src/modules/hecate/composables/createCommonImportResolvers.ts
+++ apps/ui/src/modules/hecate/composables/createCommonImportResolvers.ts
@@ -1,5 +1,6 @@
 import type {  HecateCompilerImportResolver } from "hecate/composables/createCompiler";
 import { defineImportResolver } from "hecate/composables/defineImportResolver";
+import { useDrive } from "@/composables/useDrive";
 
 export interface Options {
     
@@ -14,6 +15,10 @@ export function createCommonImportResolvers(options?: Options) {
         defineImportResolver({
             test: (path: string) => path === 'vue',
             resolve: async () => import('vue')
+        }),
+        defineImportResolver({
+            test: (path: string) => path === 'app:drive',
+            resolve: async () => Promise.resolve({ useDrive })
         })
     )
 
