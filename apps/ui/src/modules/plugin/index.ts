6fc58fb50725d6db8ef79ba7da13cbd633d75097
Henrique de Oliveira <henriqueoliwork@gmail.com>
2024-08-17 18:58:07 -0300

    feat: temporay disable plugins load

--- apps/ui/src/modules/plugin/index.ts
+++ apps/ui/src/modules/plugin/index.ts
@@ -3,20 +3,20 @@ import PluginSidebar from './components/PluginSidebar.vue'
 
 export default defineAppModule({
     async setup({ addAppPage, addMenuItem }) {
-		addAppPage({
-			name: 'Plugins',
-			component: PluginListAppPage 
-		})
-
-        addMenuItem({
-            name: 'plugins',
-            order: 1,
-            label: 'Plugins',
-            icon: 'heroicons:rocket-launch-solid',
-            component: PluginSidebar,
-        })
-
-		onHook('workspace:loaded', loadActivePlugins)
+//		addAppPage({
+//			name: 'Plugins',
+//			component: PluginListAppPage 
+//		})
+//
+//        addMenuItem({
+//            name: 'plugins',
+//            order: 1,
+//            label: 'Plugins',
+//            icon: 'heroicons:rocket-launch-solid',
+//            component: PluginSidebar,
+//        })
+//
+//		onHook('workspace:loaded', loadActivePlugins)
 	
     }
 })
