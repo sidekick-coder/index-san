666e40b80cb85080becaa5d0f958b240d5471594
Henrique de Oliveira <henriqueoliwork@gmail.com>
2024-08-24 17:42:40 -0300

    feat(docs): add hephaestus charts doc

--- apps/docs/app.config.ts
+++ apps/docs/app.config.ts
@@ -1,39 +1,33 @@
 // https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
 export default defineAppConfig({
-  docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
-    socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
-      nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      }
+    docus: {
+        title: 'Index-san',
+        description: 'Powerfull note-taking with the power of coding',
+        image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+        socials: {
+            github: 'sidekick-coder/index-san',
+        },
+        github: {
+            dir: 'apps/docs',
+            branch: 'main',
+            repo: 'index-san',
+            owner: 'sidekick-coder',
+            edit: true,
+        },
+        aside: {
+            level: 0,
+            collapsed: false,
+            exclude: [],
+        },
+        main: {
+            padded: true,
+            fluid: true,
+        },
+        header: {
+            logo: true,
+            showLinkIcon: true,
+            exclude: [],
+            fluid: true,
+        },
     },
-    github: {
-      dir: '.starters/default/content',
-      branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
-      edit: true
-    },
-    aside: {
-      level: 0,
-      collapsed: false,
-      exclude: []
-    },
-    main: {
-      padded: true,
-      fluid: true
-    },
-    header: {
-      logo: true,
-      showLinkIcon: true,
-      exclude: [],
-      fluid: true
-    }
-  }
 })
