51e9d07009ad1f30feff448357c09a78e6408af5
Henrique de Oliveira <henriqueoliwork@gmail.com>
2024-09-07 16:15:42 -0300

    feat(ui): add chart json option

--- apps/ui/src/components/IsChart.vue
+++ apps/ui/src/components/IsChart.vue
@@ -2,28 +2,52 @@
 import ApexCharts from 'apexcharts'
 
 const options = defineModel({
-    type: Object,
-    default: null
+	type: Object,
+	default: null
 })
 
 const root = ref<HTMLElement | null>(null)
+const slots = useSlots()
 
 let chart: ApexCharts | null = null
 
-function load(){
-    if (!options.value) return
-    if (!root.value) return
+function loadFromProp() {
+	chart = new ApexCharts(root.value, options.value);
 
-    if (chart) {
-        chart.destroy()
-    }
-
-    chart = new ApexCharts(root.value, options.value);
-    
-    chart.render();
+	chart.render();
 }
 
-watch(options, load, { immediate: true })
+function loadFromSlot() {
+	const text = slots.default!()[0].children as string
+
+	const json = JSON.parse(text)
+
+	chart = new ApexCharts(root.value, json);
+
+	chart.render();
+}
+
+
+function load() {
+	if (!root.value) return
+
+	if (chart) {
+		chart.destroy()
+	}
+
+	if (options.value) {
+		return loadFromProp()
+	}
+
+	if (slots.default) {
+		return loadFromSlot()
+	}
+
+}
+
+onMounted(load)
+
+watch(options, load)
 </script>
 
 <template>
